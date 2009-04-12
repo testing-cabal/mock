c510400dea4442dd08d49f5fd0ba04c06718f8ed
fuzzyman <fuzzyman@localhost>
2009-04-12 22:47:36 +0000

    updating documentation and copyright year in sources

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-# Copyright (C) 2007-2008 Michael Foord
+# Copyright (C) 2007-2009 Michael Foord
 # E-mail: fuzzyman AT voidspace DOT org DOT uk
 # http://www.voidspace.org.uk/python/mock/
 
