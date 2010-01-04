6be478bc702e139cdf3ab5d0dae4217c80549da5
fuzzyman <fuzzyman@localhost>
2010-01-04 13:45:38 +0000

    Refactoring tests

--- tests/testwith.py
+++ tests/testwith.py
@@ -77,6 +77,17 @@ class WithTest(TestCase):
             self.assertEquals(mock_something, sentinel.Patched, "wrong patch")        
         self.assertEquals(something, sentinel.Something)
 
+    
+    def testContextManagerMocking(self):
+        mock = Mock()
+        mock.__enter__ = Mock()
+        mock.__exit__ = Mock()
+        mock.__exit__.return_value = False
+        
+        with mock as m:
+            self.assertEqual(m, mock.__enter__.return_value)
+        mock.__enter__.assert_called_with()
+        mock.__exit__.assert_called_with(None, None, None)
 
 
 if __name__ == '__main__':
