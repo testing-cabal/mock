f7cf4c0f73c6b478c3d7e5f97c868be3e67d498e
fuzzyman <fuzzyman@localhost>
2010-06-12 12:49:42 +0000

    Text formatting in documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ copyright = u'2009-2010, Michael Foord'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.6.0'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.6.0'
+release = '0.7.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
