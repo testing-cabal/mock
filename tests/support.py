141d5db0a2d46e5df17babb7de581b831ccfece4
konryd <konryd@localhost>
2010-06-14 10:29:08 +0000

    made the tests slightly less evil (I couldn't sleep because of that :D )

--- tests/support.py
+++ tests/support.py
@@ -1,5 +1,7 @@
 import warnings
 
 def examine_warnings(func):
-    with warnings.catch_warnings(record=True) as ws:
-        func(ws)
+    def wrapper():
+        with warnings.catch_warnings(record=True) as ws:
+            func(ws)
+    return wrapper
