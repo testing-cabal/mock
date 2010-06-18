6133fdd924f6f12cc79ae3db1bc124c956d9ae22
fuzzyman <fuzzyman@localhost>
2010-06-18 11:21:03 +0000

    Update copyright notices in code.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-# Copyright (C) 2007-2010 Michael Foord
+# Copyright (C) 2007-2010 Michael Foord & the mock team
 # E-mail: fuzzyman AT voidspace DOT org DOT uk
 # http://www.voidspace.org.uk/python/mock/
 
