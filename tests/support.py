7a2cfa8700df4342ee082141fbed1f62c4af56e0
fuzzyman <fuzzyman@localhost>
2010-08-02 22:25:32 +0000

    updating tests to use unittest2 on Python 2.7

--- tests/support.py
+++ tests/support.py
@@ -1,7 +1,7 @@
 import sys
 
 info = sys.version_info
-if info[:3] >= (3, 2, 0) or info[0] == 2 and info[1] >= 7:
+if info[:3] >= (3, 2, 0):
     # for Python 2.7 and 3.2 ordinary unittest is fine
     import unittest as unittest2
 else:
