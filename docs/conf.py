16ace4ec30cbc25f20830c2a8ce81512b224cd40
fuzzyman <fuzzyman@localhost>
2010-09-18 21:01:20 +0000

    Changes for 0.7.0b3 release

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ copyright = u'2009-2010, Michael Foord & the mock team'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.7.0'
+version = '0.7.0b3'
 # The full version, including alpha/beta/rc tags.
-release = '0.7.0'
+release = '0.7.0b3'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
