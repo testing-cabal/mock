f49a086e8c3aaf6bba8c580c3ffb33191f21af51
Michael Foord <michael@voidspace.org.uk>
2011-02-17 10:46:59 +0000

    Changing dates in copyright notices

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Copyright (C) 2007-2010 Michael Foord & the mock team
+# Copyright (C) 2007-2011 Michael Foord & the mock team
 # E-mail: fuzzyman AT voidspace DOT org DOT uk
 # http://www.voidspace.org.uk/python/mock/
 
