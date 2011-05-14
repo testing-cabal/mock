4e9b08239c97057a526b124afc7f8d2a1568140c
Michael Foord <michael@python.org>
2011-05-14 11:59:11 +0200

    Testing call

--- tests/testhelpers.py
+++ tests/testhelpers.py
@@ -4,7 +4,7 @@
 
 from tests.support import unittest2
 
-from mock import Mock, ANY
+from mock import Mock, ANY, call
 
 
 class AnyTest(unittest2.TestCase):
@@ -23,3 +23,51 @@ class AnyTest(unittest2.TestCase):
     def test_repr(self):
         self.assertEqual(repr(ANY), '<ANY>')
         self.assertEqual(str(ANY), '<ANY>')
+
+
+
+class CallTest(unittest2.TestCase):
+    """
+    >>> mock = Mock(return_value=None)
+    >>> mock(1, 2, 3)
+    >>> mock(a=3, b=6)
+    >>> mock.call_args_list == [call(1, 2, 3), call(a=3, b=6)]
+    True
+
+    >>> mock = Mock()
+    >>> mock.foo(1, 2 ,3)
+    <mock.Mock object at 0x...>
+    >>> mock.bar.baz(a=3, b=6)
+    <mock.Mock object at 0x...>
+    >>> mock.method_calls == [call.foo(1, 2, 3), call.bar.baz(a=3, b=6)]
+    True
+
+And for good measure, the first example (tracking order of calls between
+mocks) using the new `call` object for assertions:
+
+.. doctest::
+
+    >>> manager = Mock()
+
+    >>> mock_foo = manager.foo
+    >>> mock_bar = manager.bar
+
+    >>> mock_foo.something()
+    <mock.Mock object at 0x...>
+    >>> mock_bar.other.thing()
+    <mock.Mock object at 0x...>
+
+    >>> manager.method_calls == [call.foo.something(), call.bar.other.thing()]
+    True
+    """
+
+    def test_repr(self):
+        self.assertEqual(repr(call), '<call>')
+        self.assertEqual(str(call), '<call>')
+
+        self.assertEqual(repr(call.foo), '<call foo>')
+
+    def test_call(self):
+        self.assertEqual(call(), ((), {}))
+        self.assertEqual(call('foo', 'bar', one=3, two=4),
+                         (('foo', 'bar'), {'one': 3, 'two': 4}))
