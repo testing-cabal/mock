5c388c744a2d9aca95a0bdf52234477546470da3
Michael Foord <michael@voidspace.org.uk>
2011-08-10 18:48:48 +0100

    Initial implementation of mock_add_spec

--- tests/testmock.py
+++ tests/testmock.py
@@ -1083,6 +1083,74 @@ class MockTest(unittest2.TestCase):
             )
 
 
+    def test_mock_add_spec(self):
+        class _One(object):
+            one = 1
+        class _Two(object):
+            two = 2
+        class Anything(object):
+            one = two = three = 'four'
+
+        klasses = [
+            Mock, MagicMock, NonCallableMock, NonCallableMagicMock
+        ]
+        for Klass in list(klasses):
+            klasses.append(lambda K=Klass: K(spec=Anything))
+            klasses.append(lambda K=Klass: K(spec_set=Anything))
+
+        for Klass in klasses:
+            for kwargs in dict(), dict(spec_set=True):
+                mock = Klass()
+                #no error
+                mock.one, mock.two, mock.three
+
+                for One, Two in [(_One, _Two), (['one'], ['two'])]:
+                    for kwargs in dict(), dict(spec_set=True):
+                        mock.mock_add_spec(One, **kwargs)
+
+                        mock.one
+                        self.assertRaises(
+                            AttributeError, getattr, mock, 'two'
+                        )
+                        self.assertRaises(
+                            AttributeError, getattr, mock, 'three'
+                        )
+                        if 'spec_set' in kwargs:
+                            self.assertRaises(
+                                AttributeError, setattr, mock, 'three', None
+                            )
+
+                        mock.mock_add_spec(Two, **kwargs)
+                        self.assertRaises(
+                            AttributeError, getattr, mock, 'one'
+                        )
+                        mock.two
+                        self.assertRaises(
+                            AttributeError, getattr, mock, 'three'
+                        )
+                        if 'spec_set' in kwargs:
+                            self.assertRaises(
+                                AttributeError, setattr, mock, 'three', None
+                            )
+            # note that creating a mock, setting an instance attribute, and
+            # *then* setting a spec doesn't work. Not the intended use case
+
+
+    def DONTtest_mock_add_spec_magic_methods(self):
+        mock = MagicMock()
+        int(mock)
+
+        mock.mock_add_spec(object)
+        self.assertRaises(TypeError, int, mock)
+
+        mock = MagicMock()
+        mock['foo']
+
+        mock.mock_add_spec(int)
+        int(mock)
+        self.assertRaises(TypeError, lambda: mock['foo'])
+
+
 
 if __name__ == '__main__':
     unittest2.main()
