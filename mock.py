1afaa352a31ca02cbca0911d018726a5fce49329
Michael Foord <michael@voidspace.org.uk>
2011-10-08 01:13:25 +0100

    Setting a mock as a return value of itself no longer modifies repr of the mock

--- mock.py
+++ mock.py
@@ -471,6 +471,14 @@ def _check_and_set_parent(parent, value, name, new_name):
         (value._mock_new_parent is not None)):
         return False
 
+    _parent = parent
+    while _parent is not None:
+        # setting a mock (value) as a child or return value of itself
+        # should not modify the mock
+        if _parent is value:
+            return False
+        _parent = _parent._mock_new_parent
+
     if new_name:
         value._mock_new_parent = parent
         value._mock_new_name = new_name
