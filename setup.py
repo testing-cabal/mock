85682ac84bc59fc7c1f35e2c3a9ffe72114f3c96
Michael Foord <michael@voidspace.org.uk>
2011-12-29 11:55:42 +0000

    Doc and setup.py update

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.5',
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.0',
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: Implementation :: CPython',
