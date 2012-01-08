3c4e510b338afd03bd4c502391bd3645ddc9df72
Michael Foord <michael@voidspace.org.uk>
2012-01-08 21:00:44 +0000

    Change Mock theme

--- docs/conf.py
+++ docs/conf.py
@@ -55,11 +55,11 @@ doctest_global_cleanup = """
 sys.modules['__main__'] = sys.modules['__main']
 """
 
-html_theme = 'basic'
+html_theme = 'nature'
 html_theme_options = {}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.txt'
@@ -116,7 +116,7 @@ pygments_style = 'friendly'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'adctheme.css'
+#html_style = 'adctheme.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -137,7 +137,7 @@ html_style = 'adctheme.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
