dc1458aa1414b29d95a5d297db254a38501fff79
Michael Foord <michael@voidspace.org.uk>
2012-10-07 18:43:23 +0100

    Final doc updates for 1.0 release

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Programming Language :: Python :: Implementation :: Jython',
