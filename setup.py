30d1135dd886a5facaa7048906d11169c52b61d0
Rob McQueen <rob@nylas.com>
2015-07-10 17:04:04 -0700

    Abort installation if the installer is using setuptools<17.1

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+from distutils.version import StrictVersion
 import setuptools
+import sys
 
+
+# Setuptools 17.1 is required, and setup_requires cannot upgrade setuptools
+# in-place, nor trigger the use of a newer version. Abort cleanly up-front.
+setuptools_required = StrictVersion("17.1")
+setuptools_installed = StrictVersion(setuptools.version.__version__)
+if setuptools_installed < setuptools_required:
+    sys.stderr.write(
+        "mock requires setuptools>=17.1. Aborting installation\n")
+    sys.exit(1)
 
 setuptools.setup(
     setup_requires=['pbr>=1.3'],
