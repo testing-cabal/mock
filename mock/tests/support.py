1921419e3fdae8cd3531692ae90ecdfc50e4fcf5
Chris Withers <chris@withers.org>
2019-04-29 07:27:07 +0100

    bring uncache helper across from cpython

--- mock/tests/support.py
+++ mock/tests/support.py
@@ -1,3 +1,7 @@
+import contextlib
+import sys
+
+
 def is_instance(obj, klass):
     """Version of is_instance that doesn't access __class__"""
     return issubclass(type(obj), klass)
@@ -12,3 +16,29 @@ class SomeClass(object):
 
 class X(object):
     pass
+
+
+@contextlib.contextmanager
+def uncache(*names):
+    """Uncache a module from sys.modules.
+
+    A basic sanity check is performed to prevent uncaching modules that either
+    cannot/shouldn't be uncached.
+
+    """
+    for name in names:
+        if name in ('sys', 'marshal', 'imp'):
+            raise ValueError(
+                "cannot uncache {0}".format(name))
+        try:
+            del sys.modules[name]
+        except KeyError:
+            pass
+    try:
+        yield
+    finally:
+        for name in names:
+            try:
+                del sys.modules[name]
+            except KeyError:
+                pass
