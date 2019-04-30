127ec6f6a083108b498ab88a4dfbba43c469e66f
Chris Withers <chris@withers.org>
2019-04-30 08:10:59 +0100

    no subtest on Py2.

--- mock/tests/testmock.py
+++ mock/tests/testmock.py
@@ -1493,7 +1493,6 @@ class MockTest(unittest.TestCase):
             def static_method():
                 pass
         for method in ('class_method', 'static_method'):
-            with self.subTest(method=method):
                 mock_method = mock.create_autospec(getattr(TestClass, method))
                 mock_method()
                 mock_method.assert_called_once_with()
