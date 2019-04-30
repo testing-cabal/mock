23ff2c2ffe3ff8d17800e2c86f8a914853ffcdde
Chris Withers <chris@withers.org>
2019-04-30 08:36:13 +0100

    Deferring fixing this into https://github.com/testing-cabal/mock/issues/452

--- mock/tests/testhelpers.py
+++ mock/tests/testhelpers.py
@@ -977,6 +977,8 @@ class SpecSignatureTest(unittest.TestCase):
         check_data_descriptor(foo.desc)
 
 
+    @unittest.skipIf('PyPy' in sys.version and sys.version_info > (3, 0),
+                     "See https://github.com/testing-cabal/mock/issues/452")
     def test_autospec_on_bound_builtin_function(self):
         meth = six.create_bound_method(time.ctime, time.time())
         self.assertIsInstance(meth(), str)
