4195207c56eaa0f1707d1e64acc48967554f115f
Chris Withers <chris@withers.org>
2020-01-13 08:11:28 +0000

    support pre-releases in version_info

--- mock/__init__.py
+++ mock/__init__.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import
+
+import re
+
 import mock.mock as _mock
 from mock.mock import *
 
 __version__ = '4.0.0b1'
-version_info = tuple(int(p) for p in __version__.split('.'))
+version_info = tuple(int(p) for p in
+                     re.match(r'(\d+).(\d+).(\d+)', __version__).groups())
 
 
 __all__ = ('__version__', 'version_info') + _mock.__all__
