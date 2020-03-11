b5ce0a5c4d372b77deff46fec8edf974d7d1f875
Chris Withers <chris@simplistix.co.uk>
2020-03-11 17:10:35 +0000

    Add intersphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,13 @@ import mock
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.doctest']
+extensions = [
+    'sphinx.ext.intersphinx',
+    ]
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/dev', None),
+}
 
 doctest_global_setup = """
 import os
